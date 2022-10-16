f75aba608263897bfe0e7e44bb4e1f54e286ae23
dsalle03 <dsalle2003@gmail.com>
2022-10-16 10:20:09 +0200

    Commit 2

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,9 +1,21 @@
-from re import X
-from tkinter import Y
-
-
 class punto:
-    X
-    Y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def constructor(a=0,b=0):
+        punto = [a, b]
+        return punto
 
+    def string(punto):
+        print("("+str(punto[0])+", "+str(punto[1])+")")
 
+    def cuadrante(punto):
+        if punto[0]>0 and punto[1]>0:
+            print("El punto esta en el primer cuadrante")
+        elif punto[0]<0 and punto[1]>0:
+            print("El punto esta en el segundo cuadrante")
+        elif punto[0]>0 and punto[1]<0:
+            print("El punto esta en el tercer cuadrante")
+        elif punto[0]<0 and punto[1]<0:
+            print("El punto esta en el cuarto cuadrante")
+ 
--- README.md
+++ README.md
@@ -74,7 +74,7 @@ Añadir como primer elemento de la lista la suma realizada.
 Devolver la lista modificada.
 Finalmente, después de ejecutar la función, comprueba que la suma de todos los números a partir del segundo, concuerda con el primer número de la lista, tal que así:
 nueva_lista = modificar(lista)
-print( nueva_lista[0] == sum(nueva_lista[1:]) )
+print(nueva_lista[0] == sum(nueva_lista[1:]))
 True
 Recordatorio
 La función sum(lista) devuelve una suma de los elementos de una lista.
